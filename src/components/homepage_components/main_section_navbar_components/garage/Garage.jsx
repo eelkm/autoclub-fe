32cdc9a1b4925a802f7b89b254a33fb265bcd4c3
Edgars <edgarsapinis1@gmail.com>
2023-12-16 16:28:19 +0100

    Added dynamic car data and fetch user cars from backend

--- src/components/homepage_components/main_section_navbar_components/garage/Garage.jsx
+++ src/components/homepage_components/main_section_navbar_components/garage/Garage.jsx
@@ -2,14 +2,43 @@ import styles from './Garage.module.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import CarCard from '../../../ui_components/garage_car_card/CarCard';
+import { BackendURL } from '../../../../utils/constants';
+import { useGlobalContext } from '../../../../contexts/GlobalContext';
 
 const Garage = ({userData}) => {
+  const { token } = useGlobalContext();
+  const { currentUser } = useGlobalContext();
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BackendURL}/users/user_cars?username=${userData.username}`, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        const data = response.data;
+        if (data.success) {
+          setCars(data.cars);
+        } else {
+          console.error('Error:', data.error);
+        }
+      })
+      .catch((error) => {
+        console.error('Failed to fetch user cars:', error);
+      });
+  }, [userData, token]);
 
   return (
     <div>
-
-    <CarCard username={userData.username} year='1998'/>
     
+    {cars.map((item, index) => (
+      <CarCard key={index} username={userData.username} car={item} />
+    ))}
+
+    {cars.length === 0 && <div className={styles.no_cars}>No cars to show.</div>}
+    {currentUser === userData.username && <div className={styles.no_cars}>Add a car to your garage.</div>}
 
     </div>
   );
--- src/components/ui_components/garage_car_card/CarCard.jsx
+++ src/components/ui_components/garage_car_card/CarCard.jsx
@@ -4,8 +4,7 @@ import { useGlobalContext } from '../../../contexts/GlobalContext';
 import { HiDotsHorizontal } from 'react-icons/hi';
 
 
-const CarCard = ({username}) => {
-  const { token } = useGlobalContext();
+const CarCard = ({username, car}) => {
   const { currentUser } = useGlobalContext();
 
   const [isEditOverlayOpen, setIsEditOverlayOpen] = useState(false);
@@ -14,9 +13,9 @@ const CarCard = ({username}) => {
   return (
     <div className={styles.box} onMouseEnter={()=>(setIsHovering(true))} onMouseLeave={()=>(setIsHovering(false))}>
       <div className={styles.inside_box}>
-        <img src={'https://www.bhmpics.com/downloads/honda-prelude-modified/4.modified-honda-prelude-5-5.jpg'} alt="" className={styles.car_cover} />
+        <img src={car.first_photo} alt="" className={styles.car_cover} />
 
-        <img src={'https://www.bhmpics.com/downloads/honda-prelude-modified/4.modified-honda-prelude-5-5.jpg'} alt="" className={`${styles.car_cover_underlay} ${isHovering && styles.show}`} />
+        <img src={car.first_photo} alt="" className={`${styles.car_cover_underlay} ${isHovering && styles.show}`} />
 
         <div className={styles.gradient_box}></div>
 
@@ -27,20 +26,20 @@ const CarCard = ({username}) => {
         
 
         <div className={styles.text_box}>
-          <p className={styles.car_year}>1998</p>
-          <p className={styles.car_name}>Honda Prelude</p>
-          <p className={styles.car_description}>The 1998 Honda Prelude is a sleek and sporty two-door coupe, celebrated for its agile handling and reliability. Powered by a 2.2-liter, 4-cylinder engine with VTEC technology, it strikes a balance between performance and fuel efficiency..</p>
+          <p className={styles.car_year}>{car.year}</p>
+          <p className={styles.car_name}>{car.make_model}</p>
+          <p className={styles.car_description}>{car.desc_car}</p>
           <a href="#" className={styles.car_action}>
             <svg stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
             </svg>
-            0
+            {car.likes}
           </a>
           <a href="#" className={styles.car_action}>
             <svg stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round" className="css-i6dzq1" viewBox="0 0 24 24">
               <path d="M21 15a2 2 0 01-2 2H7l-4 4V5a2 2 0 012-2h14a2 2 0 012 2z" />
             </svg>
-            0
+            {car.comment_count}
           </a>
         </div>
 
